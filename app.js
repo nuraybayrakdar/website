1f11f5a48eba96d37f294933007d5f3eb524d501
nuraybayrakdar <bayrakdarnuray@gmail.com>
2024-08-20 14:27:04 +0300

    log eklendi

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 document.getElementById('clickButton').addEventListener('click', function() {
     document.getElementById('message').textContent = 'Butona tıkladınız!';
+    console.log('Butona tıkladınız');
 });
